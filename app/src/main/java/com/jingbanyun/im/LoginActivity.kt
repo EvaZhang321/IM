5ef7add9243860f410f1d4a69f5b6745dfa8d2e1
EvaZhang321 <1049777592@qq.com>
2018-07-11 20:00:54 +0800

    1.splash界面开发 2.环信sdk接入

---
+++ app/src/main/java/com/jingbanyun/im/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.jingbanyun.im
+
+class LoginActivity:BaseActivity() {
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_login
+    }
+}
---
+++ app/src/main/java/com/jingbanyun/im/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.jingbanyun.im
+
+import android.os.Handler
+import com.jingbanyun.im.contract.SplashContract
+import com.jingbanyun.im.presenter.SplashPresenter
+import org.jetbrains.anko.startActivity
+
+class SplashActivity:BaseActivity(),SplashContract.View {
+
+    val presenter = SplashPresenter(this)
+
+    companion object {
+     const val DELAY = 2000L
+    }
+
+    private val handler by lazy {
+        Handler()
+    }
+
+    override fun init() {
+        super.init()
+        presenter.checkLoginStatus()
+    }
+
+    //延时2秒，跳转到登录界面
+    override fun onNotLoggedIn() {
+        handler.postDelayed({
+            startActivity<LoginActivity>()
+            finish()
+        },DELAY)
+    }
+
+    //跳转到主界面
+    override fun onLoggedIn() {
+        startActivity<MainActivity>()
+        finish()
+    }
+
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_splash
+    }
+}
